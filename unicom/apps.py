3839ae4ecb7c4966b898cc6582073ca3ef1a3d36
Meena Erian Awad Hanna <menas@insightifyr.portacode.com>
2025-06-12 16:24:33 +0000

    ensure IMAP_thread_manager only runs with the runserver command =

--- unicom/apps.py
+++ unicom/apps.py
@@ -1,10 +1,12 @@
 from django.apps import AppConfig
+import sys
 
 
 class UnicomConfig(AppConfig):
     name = 'unicom'
 
     def ready(self):
-        from unicom.services.email.IMAP_thread_manager import imap_manager
+        if len(sys.argv) > 1 and sys.argv[1] == "runserver":
+            from unicom.services.email.IMAP_thread_manager import imap_manager
+            imap_manager.start_all()
         import unicom.signals
-        imap_manager.start_all()
